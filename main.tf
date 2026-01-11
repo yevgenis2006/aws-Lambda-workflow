0a2c7e0832dc8425d7ceb5c8eff94638117ba3a1
Yevgeni Shapiro <23049337+yevgenis2006@users.noreply.github.com>
2026-01-11 15:15:12 +0200

    update

--- main.tf
+++ main.tf
@@ -84,7 +84,8 @@ resource "aws_route_table" "public" {
 
 # --- Associate Public Subnets ---
 resource "aws_route_table_association" "public_assoc" {
-  count          = 2
+  count          = length(aws_subnet.public)
+  #count         = 2
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
